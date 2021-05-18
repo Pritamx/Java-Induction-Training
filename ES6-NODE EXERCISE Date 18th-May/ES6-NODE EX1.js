369c690b746cb3fa0d216cf9ce9b567d8294a510
Pritam Saha <30571354+Pritamx@users.noreply.github.com>
2021-05-18 18:13:04 +0530

    Add files via upload

---
+++ ES6-NODE EXERCISE Date 18th-May/ES6-NODE EX1.js
@@ -0,0 +1,4 @@
+//Creating arrow function to calculate square of a numbers
+let square = (x) => {return x*x}
+
+console.log("Square of 6 is "+square(6))
---
+++ ES6-NODE EXERCISE Date 18th-May/ES6-NODE EX2.js
@@ -0,0 +1,3 @@
+//Creating arrow function to calculate area of rectangle
+let area=(length,breadth) => {return length*breadth}
+console.log("Area of rectangle of length 5 and breadth 4 is "+area(5,4))
